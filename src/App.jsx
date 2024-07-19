f1c87318f124e8919997765c8444ceedbc2a2859
nagisora <nagisora.icanspeng@gmail.com>
2024-07-19 20:41:24 +0900

    自力完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const initialRecords = [
+    { title: "勉強の記録1", time: 1},
+    { title: "勉強の記録2", time: 3},
+    { title: "勉強の記録3", time: 5}
+  ]
+
+  const [records, setRecords] = useState(initialRecords);
+  const [studySession, setStudySession] = useState("");
+  const [studyTime, setStudyTime] = useState(0);
+  const [totalTime, setTotalTime] = useState(0);
+  const [isIncomplete, setIsIncomplete] = useState(false);
+
+  const onChangeStudySession = (event) => setStudySession(event.target.value);
+  const onChangeStudyTime = (event) => setStudyTime(event.target.value);
+
+  const onClickSubmit = () => {
+    // エラーハンドリング
+    const isFormIncomplete = studySession === "" || studyTime <= 0;
+    setIsIncomplete(isFormIncomplete);
+    if(isFormIncomplete) return;
+
+    // record登録
+    const newRecord = { title: studySession, time: studyTime};
+    const newRecords = [...records, newRecord];
+    setRecords(newRecords);
+
+    setStudySession("");
+    setStudyTime(0);
+
+    // 合計時間計算
+    var sumTime = 0;
+    records.forEach((record) => {
+      sumTime += parseInt(record.time, 10);
+    });
+    setTotalTime(sumTime);
+  };
+
+  // const isIncomplete = studySession === null || studyTime <= 0;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>登録</h2>
+      <div>学習内容<input type="text" value={studySession} onChange={onChangeStudySession}/></div>
+      <div>学習時間<input type="number" value={studyTime} onChange={onChangeStudyTime}/>時間</div>
+      <div>入力されている学習内容：{studySession}</div>
+      <div>入力されている時間：{studyTime}時間</div>
+      <button onClick={onClickSubmit}>登録</button>
+      {isIncomplete && <div>入力されていない項目があります</div>}
+      <div>合計時間：{totalTime}/1000(h)</div>
+      <hr/>
+      <h2>一覧</h2>
+      <ul>
+        {records.map((record, index) => (
+          <li key={index}>{record.title}：{record.time}時間</li>
+        ))}
+      </ul>
     </>
   )
 }
